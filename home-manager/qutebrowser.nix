6842c36987e089623207dc87abfb18beee6e9338
Oni <oni@onigiribentou.com>
2024-07-26 19:58:33 +0300

    Added yomichad to qutebrowser userscripts

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -25,6 +25,88 @@
     hash = "sha256-jckaJCUv3g0yvNXawI0faiI0zs1H55gpWKq1uSf8QxI=";
     postFetch = ''
       sed -i 's/hideForYouTimeline: true/hideForYouTimeline: false/g' $out
+  yomichad = pkgs.python3Packages.buildPythonPackage {
+    name = "yomichad";
+    src = pkgs.fetchFromGitHub {
+      owner = "potamides";
+      repo = "yomichad";
+      rev = "167bddba92e5dc5e581a29feb3cb56a0c4e52acf";
+      hash = "sha256-vTaq6ZoXAMgMmLOgRrx0lmR0w9QmPXQFT5UZNTZ/7MQ=";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      pyqt6
+      (buildPythonPackage {
+        pname = "puchikarui";
+        version = "0.2a2.post5";
+        src = pkgs.fetchFromGitHub {
+          owner = "letuananh";
+          repo = "puchikarui";
+          rev = "6616d6fb9f131f2489ee3067fd981a19943e095f";
+          hash = "sha256-Qct6Pdl3Li95f3LFZytmmJdQRTE3jGvLIOEtYG+p4Mo=";
+        };
+
+        doCheck = false;
+      })
+      (buildPythonPackage rec {
+        pname = "chirptext";
+        version = "0.1.2";
+        src = fetchPypi {
+          inherit pname version;
+          hash = "sha256-ZDycEVCPUJs3JX4/OmKaZowJCK5fWC17nxMSNKc3kwM=";
+        };
+
+        doCheck = false;
+      })
+      (buildPythonPackage rec {
+        pname = "jamdict";
+        version = "0.1a11.post2";
+        src = fetchPypi {
+          inherit pname version;
+          hash = "sha256-+7VN49WOzbm1NJiuhzq2UZMSyGvISKl2nZKxsjjUyRQ=";
+        };
+
+        doCheck = false;
+      })
+      (buildPythonPackage {
+        pname = "jamdict-data";
+        version = "1.5.1a2";
+        src = pkgs.fetchFromGitHub {
+          owner = "neocl";
+          repo = "jamdict_data";
+          rev = "f9c39bdfad6d60664d4f56493c4b6702ac7438f9";
+          hash = "sha256-2HOSpIcBZ4VSoyHPYL4rGodj0+ADO202/6PgxNDVkg4=";
+        };
+
+        doCheck = false;
+      })
+    ];
+
+    doCheck = false;
+
+    preBuild = ''
+
+      cat > setup.py << EOF
+      from setuptools import setup
+
+      with open('requirements.txt') as f:
+          install_requires = f.read().splitlines()
+
+      setup(
+        name='yomichad',
+        #packages=['someprogram'],
+        version='0.1.0',
+        #author='potamides'
+        #description='...',
+        install_requires=install_requires,
+        scripts=[
+          'yomichad',
+        ],
+        entry_points={
+          # example: file some_module.py -> function main
+          #'console_scripts': ['someprogram=some_module:main']
+        },
+      )
+      EOF
     '';
   };
   disable-youtube-video-ads = pkgs.fetchurl {
@@ -32,6 +114,7 @@
     hash = "sha256-/3j+aGeZ5cFuN3+1mMTquBB22702z9eX3ck6zB6kPt4=";
   };
 in {
+  home.file.".config/qutebrowser/userscripts/yomichad".source = "${yomichad}/bin/yomichad";
   programs.qutebrowser = {
     enable = true;
     loadAutoconfig = true;
