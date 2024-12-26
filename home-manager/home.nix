50f0fcef30680155a4b070f0f313b14aad2aa9a4
Oni <oni@onigiribentou.com>
2024-12-26 16:56:33 +0200

    Updated fish config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -169,7 +169,6 @@
           	end
           	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
           end, 500, Header.LEFT)
-        ''
         '';
       keymap = {
         manager = {
@@ -408,42 +407,57 @@
             sh
             */
             ''
-                set -l last_pipestatus $pipestatus
-                set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-                set -l normal (set_color normal)
-                set -q fish_color_status
-                or set -g fish_color_status red
+              set -l last_pipestatus $pipestatus
+              set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+              set -l normal (set_color normal)
+              set -q fish_color_status
+              or set -g fish_color_status red
 
-                # Color the prompt differently when we're root
-                set -l color_cwd $fish_color_cwd
-                set -l suffix '─▪'
-                if functions -q fish_is_root_user; and fish_is_root_user
-                  if set -q fish_color_cwd_root
-                    set color_cwd $fish_color_cwd_root
-                  end
-                  set suffix '#'
+              # Color the prompt differently when we're root
+              set -l color_cwd $fish_color_cwd
+              set -l suffix '─▪'
+              if functions -q fish_is_root_user; and fish_is_root_user
+                if set -q fish_color_cwd_root
+                  set color_cwd $fish_color_cwd_root
                 end
+                set suffix '#'
+              end
 
-                # Write pipestatus
-                # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-                set -l bold_flag --bold
-                set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-                if test $__fish_prompt_status_generation = $status_generation
-                  set bold_flag
+              # Write pipestatus
+              # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+              set -l bold_flag --bold
+              set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+              if test $__fish_prompt_status_generation = $status_generation
+                set bold_flag
+              end
+              set __fish_prompt_status_generation $status_generation
+              set -l status_color (set_color $fish_color_status)
+              set -l statusb_color (set_color $bold_flag $fish_color_status)
+              set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+              # Show that you are in nix-shell
+              set -l nix_shell_info (
+                if test -n "$IN_NIX_SHELL"
+                  echo -n "nix-shell "
                 end
-                set __fish_prompt_status_generation $status_generation
-                set -l status_color (set_color $fish_color_status)
-                set -l statusb_color (set_color $bold_flag $fish_color_status)
-                set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-              #
-              #   echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-              # end
+              )
+              set -l git (echo -n -s (fish_vcs_prompt | awk '{$1=$1};1') " ")
 
-                echo -n -s "$prompt_status $suffix "
+              echo -n -s "$git" (set_color $color_cwd) "$nix_shell_info$prompt_status" (set_color $color_cwd) "$suffix "
+
             '';
         };
         cheat = {
           body = "curl cheat.sh/$argv[1]";
+        };
+        octaveEnv = {
+          body =
+            /*
+            sh
+            */
+            ''
+              nix-shell -p "octave.withPackages (pkgs: with pkgs; [ $argv ])"
+            '';
         };
       };
       plugins = with pkgs.fishPlugins; [
